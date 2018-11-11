ff199ca0c4b5cb20d1ef6bac49337f4006559145
terry2tan <terry2.tan@gmail.com>
2018-11-11 15:44:05 +0800

    init

---
+++ precache-manifest.a75a3e2a6ad1028b350ffc15cddbbe2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "eb4a2e80611414e1e0fe",
+    "url": "/static/css/main.144f4fe6.chunk.css"
+  },
+  {
+    "revision": "eb4a2e80611414e1e0fe",
+    "url": "/static/js/main.eb4a2e80.chunk.js"
+  },
+  {
+    "revision": "c723f5fd0ca6274d24b1",
+    "url": "/static/js/1.c723f5fd.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d62b6e475c41bad9f4b16f650140a3e4",
+    "url": "/index.html"
+  }
+];
